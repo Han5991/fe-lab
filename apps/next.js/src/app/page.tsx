ec38984b75751a3fc208e888d0323effffacff10
sangwook <bnbt3@naver.com>
2025-04-12 12:24:19 +0900

    chore: replace TailwindCSS with PandaCSS
    - TailwindCSS 관련 설정 및 의존성 제거
    - PandaCSS 적용 및 관련 설정 추가
    - React 19.1.0 버전으로 종속성 업데이트 및 workspace 기반 패키지 구성 변경
    - 디자인 시스템의 Button 및 CSS 라이브러리 활용

--- apps/next.js/src/app/page.tsx
+++ apps/next.js/src/app/page.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
+import { css } from "@design-system/ui-lib/css";
+import { Button } from "@design-system/ui";
 
 const Page = () => (
-  <div>
+  <div className={css({ color: "blue" })}>
     <h1>Home</h1>
     <Link href="/about">link</Link>
+    <Button>count</Button>
   </div>
 );
 
