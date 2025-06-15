96f8060e77079f27586d4045cbe0ac9b23b75e21
sangwook <bnbt3@naver.com>
2025-06-15 12:39:28 +0900

    fix: blog 웹의 basePath 및 assetPrefix 설정 삭제 처리

--- apps/blog/web/next.config.ts
+++ apps/blog/web/next.config.ts
@@ -4,8 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   output: 'export',
-  basePath: '/fe-lab',
-  assetPrefix: '/fe-lab',
   trailingSlash: true,
   images: {
     unoptimized: true,
